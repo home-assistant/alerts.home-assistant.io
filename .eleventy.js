04355eb2b6d6a49c6a56d990fe16a9c508901ba5
ludeeus <ludeeus@ludeeus.dev>
2022-12-13 10:28:23 +0000

    Restructure computed files

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const fs = require("fs")
-const VersionedItem = require("./utils/versioned_item")
+const alertAsJson = require("./utils/alert_as_json")
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy(
@@ -23,26 +23,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addLiquidFilter("dateToRfc3339", (value) => value ? pluginRss.dateToRfc3339(value) : "");
   eleventyConfig.addFilter("formatGitHubUrl", (value) => {const values = value.split("/"); return `${values[3]}/${values[4]}#${values[6]}`});
 
-  eleventyConfig.addFilter("alertsAsJson", (alerts) => JSON.stringify(
-    alerts.map(alert => (
-      {
-        title: alert.data.title,
-        created: alert.data.created,
-        updated: alert.data.updated,
-        integrations: alert.data.integrations?.map(integration => new VersionedItem(integration)),
-        packages: alert.data.packages?.map(package => new VersionedItem(package)),
-        homeassistant: !alert.data.homeassistant ? undefined : new VersionedItem(
-          `homeassistant ${alert.data.homeassistant}`
-        ),
-        supervisor: !alert.data.supervisor ? undefined : new VersionedItem(
-          `supervisor ${alert.data.supervisor}`
-        ),
-        // 11ty does not yet support .markdown files, homeassistant_alerts expect .markdown so we trick it
-        filename: alert.inputPath.replace("./alerts/", "").replace(".md", ".markdown"),
-        alert_url: `https://alerts.home-assistant.io${alert.url}`
-      }
-    )
-  )));
+  eleventyConfig.addFilter("alertAsJson", (alert) => JSON.stringify(alertAsJson(alert, true)))
+  eleventyConfig.addFilter("alertsAsJson", (alerts) => JSON.stringify(alerts.map(alert => alertAsJson(alert, false))));
 
 
     eleventyConfig.on('eleventy.after', async () => {
---
+++ utils/alert_as_json.js
@@ -0,0 +1,24 @@
+const VersionedItem = require("./versioned_item")
+
+module.exports = function(alert, includeContent) {
+    const contents  = {
+        title: alert.data.title,
+        created: alert.data.created,
+        updated: alert.data.updated,
+        integrations: alert.data.integrations?.map(integration => new VersionedItem(integration)),
+        packages: alert.data.packages?.map(package => new VersionedItem(package)),
+        homeassistant: !alert.data.homeassistant ? undefined : new VersionedItem(
+          `homeassistant ${alert.data.homeassistant}`
+        ),
+        supervisor: !alert.data.supervisor ? undefined : new VersionedItem(
+          `supervisor ${alert.data.supervisor}`
+        ),
+        // 11ty does not yet support .markdown files, homeassistant_alerts expect .markdown so we trick it
+        filename: alert.inputPath.replace("./alerts/", "").replace(".md", ".markdown"),
+        alert_url: `https://alerts.home-assistant.io${alert.url}`
+      }
+    if (includeContent) {
+        contents["content"] = alert.template.frontMatter.content
+    }
+    return contents
+  }
